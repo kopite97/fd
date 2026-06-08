f5befd41ce20e9543625bf64e7cbdb3f9c8ae1f7
joon <tjrwns3428@gmail.com>
2026-06-08 21:10:10 +0900

    init

---
+++ src/test/java/com/kopite/fd/FdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kopite.fd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
